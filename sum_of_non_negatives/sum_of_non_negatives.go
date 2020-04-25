35baed2f507cab6bceabfbe836b48696dd6ec586
Alexander Shein <mr.ashein@gmail.com>
2020-04-25 08:43:54 +0300

    Cleanup

--- sum_of_non_negatives/sum_of_non_negatives.go
+++ sum_of_non_negatives/sum_of_non_negatives.go
@@ -4,8 +4,7 @@ import (
 	"fmt"
 )
 
-// PositiveSum does something.
-func PositiveSum(numbers []int) int {
+func positivesSum(numbers []int) int {
 	result := 0
 	for _, value := range numbers {
 		if value > 0 {
@@ -17,6 +16,5 @@ func PositiveSum(numbers []int) int {
 
 func main() {
 	numbers := []int{1, 2, 3, -1, 4, -2}
-	fmt.Println(PositiveSum(numbers))
-
+	fmt.Println(positivesSum(numbers))
 }
