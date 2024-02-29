8721b10d20481bee2dec4be9c0265ca23d0550d6
Alexander Shein <mr.ashein@gmail.com>
2024-03-01 00:03:17 +0200

    Update sudoku solver to use bit masks and todo list

--- python/sudoku_solver.py
+++ python/sudoku_solver.py
@@ -1,52 +1,114 @@
 from typing import Iterator
-import pprint as pp
 from itertools import chain
 
-ALLOWED_VALUES_SET = set(range(1, 10))
+import cProfile
+import pprint as pp
+
+
+DEFAULT_BITMASK_VALUE = 0b111111111  # 9 ones
 
 
 class InvalidPuzzleError(Exception):
     pass
 
 
-def get_box(puzzle: list[list[int]], row_index: int, col_index: int) -> list[int]:
-    row_min = (row_index // 3) * 3
-    col_min = (col_index // 3) * 3
+class SudokuSolver:
+    def __init__(self, puzzle: list[int]):
+        self.puzzle = puzzle
 
-    return [
-        *puzzle[row_min * 9 + col_min : row_min * 9 + col_min + 3],
-        *puzzle[(row_min + 1) * 9 + col_min : (row_min + 1) * 9 + col_min + 3],
-        *puzzle[(row_min + 2) * 9 + col_min : (row_min + 2) * 9 + col_min + 3],
-    ]
+        self.row_masks = [DEFAULT_BITMASK_VALUE for i in range(9)]
+        self.col_masks = [*self.row_masks]
+        self.box_masks = [*self.row_masks]
 
+        self.todo = []  # indices of cells that we need to fill
 
-def get_possible_cell_values(
-    puzzle: list[int], row_index: int, col_index: int
-) -> Iterator[int]:
-    for value in ALLOWED_VALUES_SET - {
-        *puzzle[row_index * 9 : (row_index + 1) * 9],
-        *(puzzle[_row_index * 9 + col_index] for _row_index in range(9)),
-        *get_box(puzzle, row_index, col_index),
-    }:
-        yield value
+        for row_index in range(9):
+            for col_index in range(9):
+                value = puzzle[row_index * 9 + col_index]
+                box_index = (row_index // 3) * 3 + col_index // 3
+                if value == 0:
+                    self.todo.append((row_index, col_index, box_index))
+                else:
+                    # We are setting bit number <value> to zero meaning we can't use that number anymore
+                    self.row_masks[row_index] = self.row_masks[row_index] ^ (
+                        1 << value - 1
+                    )
+                    self.col_masks[col_index] = self.col_masks[col_index] ^ (
+                        1 << value - 1
+                    )
+                    self.box_masks[box_index] = self.box_masks[box_index] ^ (
+                        1 << value - 1
+                    )
 
+    def get_possible_cell_values(self, row_index: int, col_index: int) -> Iterator[int]:
+        box_index = (row_index // 3) * 3 + col_index // 3
+        allowed_row_values_mask = (
+            self.row_masks[row_index]
+            & self.col_masks[col_index]
+            & self.box_masks[box_index]
+        )
+        for value in range(9):
+            if allowed_row_values_mask & 1 << value:
+                yield value + 1
 
-def solve(puzzle: list[int], start_row: int, zeros_count: int) -> list[list[int]]:
-    solutions = []
-    for row_index in range(start_row, 9):
-        for col_index in range(9):
-            if puzzle[row_index * 9 + col_index] == 0:
-                for new_value in get_possible_cell_values(puzzle, row_index, col_index):
-                    # We set the new value to [row_index, col_index] and reset it back after testing
-                    puzzle[row_index * 9 + col_index] = new_value
-                    if zeros_count == 1:
-                        solutions.append([*puzzle])
-                    else:
-                        solutions.extend(solve(puzzle, row_index, zeros_count - 1))
-                    if len(solutions) > 1:
-                        raise InvalidPuzzleError("Puzzle has more than 1 solution")
-                    puzzle[row_index * 9 + col_index] = 0
-                return solutions
+    def solve(self, todo_index: int, zeros_count: int) -> list[list[int]]:
+        solutions = []
+
+        row_index, col_index, box_index = self.todo[todo_index]
+        for new_value in self.get_possible_cell_values(row_index, col_index):
+            # We set the new value to [row_index, col_index] and reset it back after testing
+            self.puzzle[row_index * 9 + col_index] = new_value
+            # Updating masks with the new value
+            self.row_masks[row_index] ^= 1 << new_value - 1
+            self.col_masks[col_index] ^= 1 << new_value - 1
+            self.box_masks[box_index] ^= 1 << new_value - 1
+
+            if zeros_count == 1:
+                # The sudoku is solved
+                solutions.append([*self.puzzle])
+            elif new_solutions := self.solve(todo_index + 1, zeros_count - 1):
+                solutions.extend(new_solutions)
+            if len(solutions) > 1:
+                raise InvalidPuzzleError("Puzzle has more than 1 solution")
+            # Reseting masks
+            self.row_masks[row_index] |= 1 << new_value - 1
+            self.col_masks[col_index] |= 1 << new_value - 1
+            self.box_masks[box_index] |= 1 << new_value - 1
+
+            self.puzzle[row_index * 9 + col_index] = 0
+        return solutions
+
+    # def alternative_solve(self, start_row: int, zeros_count: int) -> list[list[int]]:
+    #     solutions = []
+    #     for row_index in range(start_row, 9):
+    #         for col_index in range(9):
+    #             box_index = (row_index // 3) * 3 + col_index // 3
+    #             # TODO (Alexander Shein) Use cell coordinates from queue
+    #             if self.puzzle[row_index * 9 + col_index] == 0:
+    #                 for new_value in self.get_possible_cell_values(
+    #                     row_index, col_index
+    #                 ):
+    #                     # We set the new value to [row_index, col_index] and reset it back after testing
+    #                     self.puzzle[row_index * 9 + col_index] = new_value
+    #                     # Updating masks with the new value
+    #                     self.row_masks[row_index] ^= 1 << new_value - 1
+    #                     self.col_masks[col_index] ^= 1 << new_value - 1
+    #                     self.box_masks[box_index] ^= 1 << new_value - 1
+
+    #                     if zeros_count == 1:
+    #                         # The sudoku is solved
+    #                         solutions.append([*self.puzzle])
+    #                     elif new_solutions := self.solve(row_index, zeros_count - 1):
+    #                         solutions.extend(new_solutions)
+    #                     if len(solutions) > 1:
+    #                         raise InvalidPuzzleError("Puzzle has more than 1 solution")
+    #                     # Reseting masks
+    #                     self.row_masks[row_index] |= 1 << new_value - 1
+    #                     self.col_masks[col_index] |= 1 << new_value - 1
+    #                     self.box_masks[box_index] |= 1 << new_value - 1
+
+    #                     self.puzzle[row_index * 9 + col_index] = 0
+    #                 return solutions
 
 
 def sudoku_solver(puzzle: list[list[int]]) -> list[list[int]]:
@@ -65,7 +127,8 @@ def sudoku_solver(puzzle: list[list[int]]) -> list[list[int]]:
                 "Cell value out of range 1~9 and 0 for empty cells"
             )
 
-    solutions = solve(_puzzle, 0, zeros_count)
+    solver = SudokuSolver(_puzzle)
+    solutions = solver.solve(0, zeros_count)
     if not solutions:
         raise InvalidPuzzleError("Unsolvable puzzle")
     return [solutions[0][offset : offset + 9] for offset in range(0, 81, 9)]
@@ -97,6 +160,9 @@ if __name__ == "__main__":
     ]
 
     solver_output = sudoku_solver(puzzle)
+
+    cProfile.run("sudoku_solver(puzzle)")
+
     print("** Solution found! **")
     pp.pprint(solver_output)
     assert solver_output == solution, "Failed to solve sample sudoku"
